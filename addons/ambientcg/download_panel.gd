f67f976e86459f8498f10c7083cc8c0539474fa1
Gordon MacPherson <gordon@gordonite.tech>
2025-04-27 01:32:20 +0100

    Fix textures not using imported resources directly as the material texture
    This ensures our material files stay small and that we don't embed the resources inside the materials.
    
    Some materials were coming out at 600 MB therefore I fixed this by loading images directly from the resource system.
    This will also ensure detection works properly and the material file always shows up

--- addons/ambientcg/download_panel.gd
+++ addons/ambientcg/download_panel.gd
@@ -24,7 +24,7 @@ func download(version):
 	download.request_raw(download_url)
 	var data : PackedByteArray = await download.request_completed
 	download.queue_free()
-	extract(path, file_name)
+	await extract(path, file_name)
 
 	downloading = false
 
@@ -51,32 +51,55 @@ func extract(zip_file: String, file_name: String):
 	prints("Extracted", zip_file)
 	download_finished.emit()
 
-	create_material(extract_path, file_name)
+	await create_material(extract_path, file_name)
 
 	DirAccess.remove_absolute(zip_file)
 
 func create_material(directory, file_name: String):
 	print("Creating Material")
+	var editor_fs: EditorFileSystem = EditorInterface.get_resource_filesystem()
 	var new_material = StandardMaterial3D.new()
-	var files = DirAccess.get_files_at(directory)
+
+	# note: dir files is relative!
+	var dir_files = DirAccess.get_files_at(directory)
+
+	# a valid file is a png or jpg, other files are ignored!
+	var valid_files: Array[String] = []
+	# ensure files are only png
+	for file in dir_files:
+		var ext = file.get_extension()
+		if ext == "jpg" or ext == "png":
+			valid_files.push_back(directory.path_join(file))
+
+	# Algorithm to force file to be loaded synchronously
+	# Forces editor to finish scanning the new imported png files, then we can just load() them!
+	for file in valid_files:
+		editor_fs.update_file(file)
+	await get_tree().process_frame
+
+	while not is_equal_approx(1.0, editor_fs.get_scanning_progress()) and editor_fs.is_scanning():
+		var progress = editor_fs.get_scanning_progress()
+		print("Waiting for initial file import progress: ", progress)
+		await get_tree().create_timer(1.0).timeout
+
+
 	var albedo_filename = ""
-	for file in files:
-		file = directory + "/" + file
+	for file in valid_files:
 		if file.contains("Color"):
-			new_material.albedo_texture = ImageTexture.create_from_image(Image.load_from_file(file))
+			new_material.albedo_texture = load(file)
 			albedo_filename = file.get_basename()
 		if file.contains("Displacement"):
 			new_material.heightmap_enabled = true
-			new_material.heightmap_texture = ImageTexture.create_from_image(Image.load_from_file(file))
+			new_material.heightmap_texture = load(file)
 
 		if file.contains("NormalGL"):
 			new_material.normal_enabled = true
-			new_material.normal_texture = ImageTexture.create_from_image(Image.load_from_file(file))
+			new_material.normal_texture = load(file)
 
 		if file.contains("Roughness"):
-			new_material.roughness_texture = ImageTexture.create_from_image(Image.load_from_file(file))
+			new_material.roughness_texture = load(file)
 		if file.contains("AmbientOcclusion"):
-			new_material.ao_texture = ImageTexture.create_from_image(Image.load_from_file(file))
+			new_material.ao_texture = load(file)
 	if new_material.albedo_texture:
 		var save_path = ""
 		if albedo_filename.is_empty():
@@ -86,10 +109,13 @@ func create_material(directory, file_name: String):
 		var uid: int = ResourceUID.create_id()
 		ResourceSaver.save(new_material, save_path)
 		ResourceUID.set_id(uid, save_path)
+		#ResourceFormatSaver.set_uid._set_uid()
 		ResourceSaver.get_resource_id_for_path(save_path, true)
 		print("Saved Material ", save_path)
 
 		EditorInterface.get_resource_filesystem().update_file(save_path)
+		EditorInterface.get_resource_filesystem().scan()
+		EditorInterface.get_resource_filesystem().scan_sources()
 		EditorInterface.get_resource_filesystem().scan()
 
 
